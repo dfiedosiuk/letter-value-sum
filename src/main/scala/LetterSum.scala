e66857920ae9c4b53a1695538a712d320686bb8a
Daniel <d.fiedosiuk@adform.com>
2022-04-01 09:02:32 +0200

    Method count working. All test passed

--- src/main/scala/LetterSum.scala
+++ src/main/scala/LetterSum.scala
@@ -1,5 +1,14 @@
 object LetterSum extends App {
+
   def count(text: String): Int = {
-    1
+    text
+      .toLowerCase
+      .toList
+      .filter(('a' to 'z').contains(_))
+      .map {
+        case c if c != ' ' => c.toInt - 96
+        case c => 0
+      }.sum
   }
+
 }
--- src/test/scala/LetterSumSpec.scala
+++ src/test/scala/LetterSumSpec.scala
@@ -9,7 +9,12 @@ class LetterSumSpec extends AnyFlatSpec with should.Matchers {
   behavior of "An LetterSum.count()"
 
   it should "return sum of letters values" in {
+    count("") shouldBe 0
     count("a") shouldBe 1
+    count("z") shouldBe 26
+    count("cab") shouldBe 6
+    count("excellent") shouldBe 100
+    count("microspectrophotometries") shouldBe 317
   }
 
 }
